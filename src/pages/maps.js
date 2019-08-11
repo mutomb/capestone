72de3691c21e4f10e1310f4b00e4ad52351341e9
Kabelo Davhana <dvhkab001@myuct.ac.za>
2019-08-11 15:28:56 +0200

    editing the table Page

--- src/pages/maps.js
+++ src/pages/maps.js
@@ -12,7 +12,7 @@ class Maps extends Component {
             <div
               id="map-container"
               className="rounded z-depth-1-half map-container"
-              style={{ height: "700px", width:"400px" }}
+              style={{ height: "700px", width:"370px" }}
             >
               <iframe
                 src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d76765.98321148289!2d-73.96694563267306!3d40.751663750099084!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spl!2spl!4v1525939514494"
